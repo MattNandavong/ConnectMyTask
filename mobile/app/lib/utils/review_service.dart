b1b1c9dcc2e3e6747235de4bf2ee8efb2e5042bb
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-04-22 19:06:36 +1000

    add reviews to profile

---
+++ mobile/app/lib/utils/review_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:app/model/Review.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Review>> getProviderReviews(String providerId) async {
+  final response = await http.get(Uri.parse('http://10.0.2.2:3300/api/reviews/$providerId'));
+
+  if (response.statusCode == 200) {
+    final List data = jsonDecode(response.body);
+    return data.map((json) => Review.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load reviews');
+  }
+}
