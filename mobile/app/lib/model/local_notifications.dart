e53ec323c3c7eacf391687ec02a352cc1e26f1ac
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-04-22 13:03:12 +1000

    add push notification for chat

--- mobile/app/lib/model/local_notifications.dart
+++ mobile/app/lib/model/local_notifications.dart
@@ -4,10 +4,13 @@ class LocalNotification {
   final DateTime timestamp;
   bool read;
 
+  final String? taskId; // nullable
+
   LocalNotification({
     required this.title,
     required this.body,
     required this.timestamp,
+    this.taskId,
     this.read = false,
   });
 
@@ -16,14 +19,16 @@ class LocalNotification {
       title: json['title'],
       body: json['body'],
       timestamp: DateTime.parse(json['timestamp']),
+      taskId: json['taskId'], // nullable
       read: json['read'] ?? false,
     );
   }
 
   Map<String, dynamic> toJson() => {
-        'title': title,
-        'body': body,
-        'timestamp': timestamp.toIso8601String(),
-        'read': read,
-      };
+    'title': title,
+    'body': body,
+    'timestamp': timestamp.toIso8601String(),
+    'taskId': taskId,
+    'read': read,
+  };
 }
--- server/index.js
+++ server/index.js
@@ -3,21 +3,26 @@ const express = require("express");
 const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 const authRoutes = require("./routes/authRoutes");
+const chatRoutes = require('./routes/chatRoutes');
+const { initIO, sendTestMessage } = require('./routes/testChatRoutes');
+
 const cors = require("cors");
 const http = require('http');
 const socketio = require('socket.io');
-const { initIO, sendTestMessage } = require('./routes/testChatRoutes');
-//chat routes
-const chatRoutes = require('./routes/chatRoutes');
 
+const admin = require("firebase-admin"); // Required for FCM
+const ChatMessage = require("./models/ChatMessage"); // Message Model
+const Task = require("./models/Task"); // Task Model for sender/receiver logic
+const User = require("./models/User"); // Needed 
 
-const ChatMessage = require('./models/ChatMessage');
-
-
+//initialize Firebase Admin SDK
+const serviceAccount = require("./config/serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 
 const app = express();
-
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
@@ -25,11 +30,12 @@ app.use(express.json());
 // Connect to Database
 connectDB();
 
-// Routes
+// Routes register
 app.use("/api/auth", authRoutes); // Use auth routes for login, register, etc.
 app.use("/api/tasks", taskRoutes); // Use task routes for task management
+app.use('/api/chat', chatRoutes);// Use chatRoutes for chat
 
-// Socket.io setup
+//Create HTTP + Socket Server
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
@@ -38,6 +44,7 @@ const io = socketio(server, {
   },
 });
 
+//initialise Socket.io comm
 io.on('connection', (socket) => {
   console.log('Socket.Io: New client connected:', socket.id);
 
@@ -46,41 +53,63 @@ io.on('connection', (socket) => {
     console.log(`Socket.Io: User joined task room: ${taskId}`);
   });
 
+  //Send messages
   socket.on('sendMessage', async (data) => {
     const messageData = {
       taskId: data.taskId,
       sender: data.sender,
       text: data.text,
     };
-  
+
+    //save message to DB
     const savedMessage = await ChatMessage.create(messageData);
-  
+
+    // Send push notification to other user
+    const task = await Task.findById(data.taskId).populate('user');
+    const recipientId = data.sender._id == task.user._id.toString() 
+      ? task.assignedProvider 
+      : task.user._id;
+
+    const recipient = await User.findById(recipientId);
+    if (recipient?.fcmToken) {
+      await admin.messaging().send({
+        token: recipient.fcmToken,
+        notification: {
+          title: 'New Chat Message',
+          body: data.text,
+        },
+        data: {
+          taskId: data.taskId,
+        },
+      });
+    }
+
+    //receive messages to client room
     io.to(data.taskId).emit('receiveMessage', {
       sender: savedMessage.sender,
       text: savedMessage.text,
       timestamp: savedMessage.timestamp,
     });
   });
-  
-  
 
   socket.on('disconnect', () => {
     console.log('Socket.Io: Client disconnected');
   });
 });
 
-const port = 3300;
 
-// Start the chat
-initIO(io);
+// Test Routes for chat using postman
 app.post("/api/test/chat", sendTestMessage);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//start server
+initIO(io);
+const port = 3300;
 server.listen(port, () => {
   console.log(`Example app listening on port hi ${port}`);
 });
 
-app.use('/api/chat', chatRoutes);
+
