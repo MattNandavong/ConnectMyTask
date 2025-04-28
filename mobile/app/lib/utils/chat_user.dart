149bea15f0a57dc02cff1e10567cb9e27c733cac
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-04-28 12:54:47 +1000

    Refactor: modified chat function to match with mohit server

--- mobile/app/lib/utils/chat_user.dart
+++ mobile/app/lib/utils/chat_user.dart
@@ -2,10 +2,11 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatService {
   late IO.Socket socket;
+  final serverUrl = 'http://192.168.1.101:3300'; // Use local IP if testing on mobile emulator
 
   void connect(String taskId, Function(dynamic) onMessageReceived) {
     socket = IO.io(
-      'http://localhost:3300', // Use local IP if testing on mobile emulator
+      serverUrl,
       IO.OptionBuilder()
           .setTransports(['websocket'])
           .disableAutoConnect()
@@ -27,6 +28,17 @@ class ChatService {
     socket.onDisconnect((_) {
       print('Disconnected from Socket.IO');
     });
+
+    socket.onError((error) {
+      print('Socket error: $error');
+    });
+
+    socket.onReconnect((_) {
+      print('Reconnected to Socket.IO');
+      socket.emit('joinTask', {'taskId': taskId}); // Rejoin the room!
+    });
+
+
   }
 
   void sendMessage(String taskId, String senderId, String text) {
