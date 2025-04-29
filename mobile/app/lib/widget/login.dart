3f30f1c547de2f5d9531ec5a86146b62df25ff29
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-04-29 17:41:15 +1000

    Refactor: create user with country base location Frontend

--- mobile/app/lib/widget/login.dart
+++ mobile/app/lib/widget/login.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:app/model/user.dart';
 import 'package:app/utils/auth_service.dart';
 import 'package:app/widget/splash_screen.dart';
-
+import 'package:country_picker/country_picker.dart';
+import 'package:geocoding/geocoding.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -24,58 +25,67 @@ class _AuthScreenState extends State<AuthScreen> {
   String _userType = 'user';
   bool _isLogin = true;
 
+
   Future<void> submit() async {
-         print('submit click');
-  final isValid = _form.currentState!.validate();
-  final token = await getFcmToken(); // Await actual token string
-  if (!isValid) return;
+    print('submit click');
+    final isValid = _form.currentState!.validate();
+    final token = await getFcmToken(); // Await actual token string
+    if (!isValid) return;
 
-  try {
-    
-    print('Tokem FCM: $token');
+    try {
+      print('Tokem FCM: $token');
 
-    User user;
-    if (_isLogin) {
-      user =await _authService.login(
-        _emailController.text.trim(),
-        _passwordController.text.trim(),
-        token!, //  Correct token here
-      );
-      Navigator.pushReplacement(
+      User user;
+      if (_isLogin) {
+        user = await _authService.login(
+          _emailController.text.trim(),
+          _passwordController.text.trim(),
+          token!, //  Correct token here
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SplashScreen()),
+        );
+      } else {
+        user = await _authService.register(
+          name: _nameController.text.trim(),
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+          role: _userType,
+          // location: {
+          //   'country': _selectedCountry ?? '',
+          //   'lat': _countryLat?.toString() ?? '',
+          //   'lng': _countryLng?.toString() ?? '',
+          // },
+          fcmToken: token,
+        );
+        print('✅ login user: ${user.id}');
+        if (!mounted) return;
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => ProfileSetupScreen(user: user)),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
         context,
-        MaterialPageRoute(builder: (context) => SplashScreen()),
-      );
-    } else {
-      user = await _authService.register(
-        name: _nameController.text.trim(),
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-        role: _userType,
-        fcmToken: token, 
-      );
-      print('✅ login user: ${user.id}');
-      if (!mounted) return;
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => ProfileSetupScreen(user: user)),
-      );
+      ).showSnackBar(SnackBar(content: Text('${e.toString()}')));
     }
-  } catch (e) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('${e.toString()}')),
-    );
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 100, 20, 100),
         child: Column(
           children: [
-            Image.asset("lib/image/connectmytask_logo.png", width: 250, height: 40),
+            Image.asset(
+              "lib/image/connectmytask_logo.png",
+              width: 250,
+              height: 40,
+            ),
             SizedBox(height: 60),
             Form(
               key: _form,
@@ -85,51 +95,72 @@ class _AuthScreenState extends State<AuthScreen> {
                     TextFormField(
                       controller: _nameController,
                       decoration: InputDecoration(labelText: 'Username'),
-                      validator: (value) => value == null || value.isEmpty
-                          ? 'Please enter a valid name'
-                          : null,
+                      validator:
+                          (value) =>
+                              value == null || value.isEmpty
+                                  ? 'Please enter a valid name'
+                                  : null,
                     ),
                   SizedBox(height: 16),
                   TextFormField(
                     controller: _emailController,
                     decoration: InputDecoration(labelText: 'Email'),
-                    validator: (value) => value == null || !value.contains('@')
-                        ? 'Enter a valid email'
-                        : null,
+                    validator:
+                        (value) =>
+                            value == null || !value.contains('@')
+                                ? 'Enter a valid email'
+                                : null,
                   ),
                   SizedBox(height: 16),
                   TextFormField(
                     controller: _passwordController,
                     obscureText: true,
                     decoration: InputDecoration(labelText: 'Password'),
-                    validator: (value) =>
-                        value != null && value.length >= 6 ? null : 'Min 6 characters',
+                    validator:
+                        (value) =>
+                            value != null && value.length >= 6
+                                ? null
+                                : 'Min 6 characters',
                   ),
                   SizedBox(height: 16),
                   if (!_isLogin)
-                    Wrap(
-                      spacing: 10,
-                      children: ['user', 'provider'].map((type) {
-                        return ChoiceChip(
-                          label: Text(type),
-                          selected: _userType == type,
-                          onSelected: (_) => setState(() => _userType = type),
-                        );
-                      }).toList(),
+                    Column(
+                      children: [
+                        
+                        SizedBox(height: 20),
+
+                        Wrap(
+                          spacing: 10,
+                          children:
+                              ['user', 'provider'].map((type) {
+                                return ChoiceChip(
+                                  label: Text(type),
+                                  selected: _userType == type,
+                                  onSelected:
+                                      (_) => setState(() => _userType = type),
+                                );
+                              }).toList(),
+                        ),
+                      ],
                     ),
+
                   SizedBox(height: 24),
-                  ElevatedButton(
-               
+                  FilledButton(
                     onPressed: submit,
                     child: Text(_isLogin ? 'Login' : 'Register'),
                   ),
                   TextButton(
                     onPressed: () => setState(() => _isLogin = !_isLogin),
-                    child: Text(_isLogin ? 'Create new account' : 'Already registered? Login'),
-                  )
+                    child: Text(
+                      _isLogin
+                          ? 'Create new account'
+                          : 'Already registered? Login',
+                      style: TextStyle(color: Colors.blueGrey),
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
