f4d04adb50c5358398849269703346c8055e9bda
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-05-07 13:18:38 +1000

    Refactor: add chat partner data
    Add chat partner data in the message screen

--- mobile/app/lib/model/chat_preview.dart
+++ mobile/app/lib/model/chat_preview.dart
@@ -2,6 +2,7 @@ class ChatPreview {
   final String taskId;
   final String taskTitle;
   final String partnerName;
+  final String? partnerId;
   final String? partnerProfilePhoto;
   final String? lastMessage;
   final String? lastImage;
@@ -11,6 +12,7 @@ class ChatPreview {
   ChatPreview({
     required this.taskId,
     required this.taskTitle,
+    this.partnerId,
     required this.partnerName,
     this.partnerProfilePhoto,
     this.lastMessage,
@@ -23,6 +25,7 @@ class ChatPreview {
     return ChatPreview(
       taskId: json['taskId'],
       taskTitle: json['taskTitle'],
+      partnerId: json['partnerId'],
       partnerName: json['partnerName'],
       partnerProfilePhoto: json['partnerProfilePhoto'],
       lastMessage: json['lastMessage'],
